002a7ffed62960214f99c3cba7f9f6bfb99e0a04
leejun <koreadlwns@naver.com>
2022-10-08 14:05:31 +0900

    third commit

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const app = express();
 app.use(cookieParser());
 
 
-// key == name, value == nodejs 인 쿠키를 보내시오
+// GET Method로 http://localhost:5001/set을 호출했을 때, name에 nodejs가 저장된 쿠키를 할당하고
 app.get("/set", (req, res) => {
   res.cookie('name', 'nodejs');
   return res.status(200).end();
 });
 
 
-// key == name, value == nodejs 인 쿠키를 받으시오
+// GET Method로http://localhost:5001/get을 호출했을 때, 쿠키에 등록된 정보들이 반환되는 API를 만들어주세요!
 app.get("/get", (req, res) => {
   const cookie = req.cookies;
   return res.status(200).json({ cookie });
