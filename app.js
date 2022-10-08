700c9a42d093bc79f14aa7f7ea96219f0363ec98
leejun <koreadlwns@naver.com>
2022-10-08 13:58:24 +0900

    second commit

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const cookieParser = require('cookie-parser');
 const app = express();
 app.use(cookieParser());
 
+
+// key == name, value == nodejs 인 쿠키를 보내시오
 app.get("/set", (req, res) => {
   res.cookie('name', 'nodejs');
   return res.status(200).end();
 });
 
+
+// key == name, value == nodejs 인 쿠키를 받으시오
 app.get("/get", (req, res) => {
   const cookie = req.cookies;
   return res.status(200).json({ cookie });
